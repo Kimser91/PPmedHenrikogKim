24d0d30abf20730086ee5cded0cae081fb53b9a3
Kimser91 <149468829+Kimser91@users.noreply.github.com>
2025-01-06 12:41:22 +0100

    first

---
+++ PPmedHenrikogKim/Main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPmedHenrikogKim
+{
+    internal class Main
+    {
+        List<Store> stores = new List<Store>()
+        {
+            new("Kiwi", "Food", 1),
+            new("Meny", "Food", 3),
+            new("Mega", "Food", 2),
+            new("ExtraLeker", "Toys", 1),
+            new("Yes!", "Toys", 3),
+            new("Toys'r us", "Toys", 2),
+            new("Lindex", "Clothes", 2),
+            new("H&M", "Clothes", 1),
+            new("LV", "Clothes", 3)
+        }; 
+         
+        public Main()
+        {
+            MainMenu();
+        }
+
+        public void MainMenu() 
+        {
+            foreach (Store store in stores) { if (store._priceLvl == 1) { Console.WriteLine(store._name); }; }
+        }
+    }
+}
---
+++ PPmedHenrikogKim/Store.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPmedHenrikogKim
+{
+    internal class Store
+    {
+        public string _name { get; private set; }
+        public string _type { get; private set; }
+        public int _priceLvl { get; private set; }
+
+        public Store(string name, string type, int priceLvl)
+        {
+            _name = name;
+            _type = type;
+            _priceLvl = priceLvl;
+        }
+    }
+}
